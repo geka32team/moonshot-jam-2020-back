23ce0aa36dddadeffefeb10df5bd4085f0bdd96d
Yevhen Drachenko <geka32@gmail.com>
2020-11-09 20:09:11 +0200

    Play with datetime() function.

--- tests/data.sql
+++ tests/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (username, password, ip_address, created_at)
 VALUES
   ('test', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f',
-    '10.0.10.11', '2018-11-08T20:18:06+00:00'),
+    '10.0.10.11', datetime('now')),
   ('other', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79',
-    '10.0.12.12', '2018-11-08T20:20:39+00:00');
+    '10.0.12.12', datetime('2018-11-08 20:20:39'));
