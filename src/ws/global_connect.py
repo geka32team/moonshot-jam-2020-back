10eb7cdb6a857d2b47559df5d2b3b17d98ef41d1
Yevhen Drachenko <geka32@gmail.com>
2020-11-20 16:28:28 +0200

    Remove unused code.

--- src/ws/global_connect.py
+++ src/ws/global_connect.py
@@ -6,8 +6,3 @@ from .namespace import Namespace as ns
 
 def handler():
     current_app.logger.debug(f"connect namespace '{request.namespace}'")
-    if request.namespace not in (
-            getattr(ns, k) for k in dir(ns) if not k.startswith('__')):
-        current_app.logger.debug(
-            f"namespace '{request.namespace}' is unavailabe")
-        raise ConnectionRefusedError('unavailabe')
