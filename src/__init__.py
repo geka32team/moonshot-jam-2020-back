5e336408ef7998a441814eeccd8ffe9151b8b9e9
Yevhen Drachenko <geka32@gmail.com>
2020-11-24 21:56:27 +0200

    Code clean-up.

--- src/__init__.py
+++ src/__init__.py
@@ -79,10 +79,4 @@ def create_app(test_config=None):
     # websockets handlers
     ws.register_all(socketio)
 
-    # make url_for('index') == url_for('blog.index')
-    # in another app, you might define a separate main index here with
-    # app.route, while giving the blog blueprint a url_prefix, but for
-    # the tutorial the blog will be the main index
-    #  app.add_url_rule("/", endpoint="index")
-
     return app
