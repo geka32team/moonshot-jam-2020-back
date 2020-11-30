dc03caa1aa21132d0df2119929abb665cce40dbc
Yevhen Drachenko <geka32@gmail.com>
2020-11-30 11:36:41 +0200

    Add and populate stats table.

---
+++ src/model/stat.py
@@ -0,0 +1,32 @@
+# pylint: disable=no-member,too-few-public-methods
+
+from ..database import db
+
+
+class Stat(db.Model):
+    __tablename__ = 'stats'
+
+    id = db.Column(db.Integer, primary_key=True)
+    lvl = db.Column(db.Integer, default=1, nullable=False)
+
+    hp = db.Column(db.Integer, default=50, nullable=False)
+
+    exp = db.Column(db.Integer, default=0, nullable=False)
+
+    strn = db.Column(db.Integer, default=7, nullable=False)
+    vit = db.Column(db.Integer, default=7, nullable=False)
+    dex = db.Column(db.Integer, default=7, nullable=False)
+    acc = db.Column(db.Integer, default=7, nullable=False)
+
+    dmg = db.Column(db.Integer, db.CheckConstraint(
+        'dmg >= 30'), default=30, nullable=False)
+
+    stats = db.Column(db.Integer, db.CheckConstraint(
+        'stats >= 0'), default=20, nullable=False)
+
+    bosses_defeated = db.Column(db.Integer, default=0, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'users.id', ondelete="CASCADE"),
+        unique=True, nullable=False)
+    user = db.relationship("User", back_populates="stat")
--- src/model/user.py
+++ src/model/user.py
@@ -8,9 +8,12 @@ from ..database import db
 
 class User(db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
     ip_address = db.Column(db.String(39), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False,
                            default=datetime.utcnow)
+
+    stat = db.relationship("Stat", uselist=False, back_populates="user")
