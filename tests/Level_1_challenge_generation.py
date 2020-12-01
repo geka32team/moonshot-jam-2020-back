c5b19f471f1fad5921443167d5a4f824c8ff1204
Sergiy Smirnov <sergiy.smirnov@sippysoft.com>
2020-12-02 00:22:02 +0200

    First func ready

--- tests/Level_1_challenge_generation.py
+++ tests/Level_1_challenge_generation.py
@@ -1,27 +1,24 @@
 """Challenge level #1.
    Returns a task in form {"task": "a +/- b = ?", "result": "x"}
 """
+
+
 import random
 
-a = random.randint(0, 9)
-b = random.randint(-5, 5)
+def level_1_challenge():
+    a = random.randint(0, 9)
+    b = random.randint(-5, 5)
+    if b < 0 :
+        sign = '-'
+    else :
+        sign = '+'
+
+    if sign == '-' and abs(b) > a :
+        a, b = b, a
+    x = abs(a+b)
+    answer = "{} {} {} = ?".format(abs(a),sign,abs(b))
+    return answer, x
 
 
-if b < 0 :
-    sign = '-'
-else :
-    sign = '+'
 
-if sign == '-' and abs(b) > a :
-    a, b = b, a
-
-
-x = abs(a+b)
-
-answer = int(input(" {} {} {} = ?\n".format(abs(a),sign,abs(b))))
-
-if answer == x :
-    print("Well Done")
-else :
-    print("Answer not correct")
-
+print(level_1_challenge())
