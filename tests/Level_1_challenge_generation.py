f986bf6b165c9b0170f4375b456ac468de331588
Sergiy Smirnov <snoff87@gmail.com>
2021-02-23 19:49:00 +0200

    challenge 1 updated levels added

--- tests/Level_1_challenge_generation.py
+++ tests/Level_1_challenge_generation.py
@@ -6,19 +6,22 @@
 
 import random
 
-def level_1_challenge():
-    a = random.randint(0, 9)
-    b = random.randint(-5, 5)
-    if b < 0 :
+def level_1_challenge(level):
+    a = random.randint(level-3, level+3)
+    b = random.choice([*range(-(level+3),-level), *range(level, level+3)])
+    if b < 0:
         sign = '-'
-    else :
+    else:
         sign = '+'
-
+    if a + b == 0:
+        a += 1
+        print("ooh la la")
     if sign == '-' and abs(b) > a :
         a, b = b, a
     x = abs(a+b)
     answer = "{} {} {} = ?".format(abs(a),sign,abs(b))
-    return answer, x
+    return answer, x, a, b
 
 
-print(level_1_challenge())
+
+print(level_1_challenge(85))
