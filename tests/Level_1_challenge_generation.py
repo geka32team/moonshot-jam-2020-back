065853cc1ed9ad83ff7eddacb84cfcaf709a36d5
Sergit Smirnov <snoff87@gmail.com>
2021-03-05 19:29:37 +0200

    now should be ok

--- tests/Level_1_challenge_generation.py
+++ tests/Level_1_challenge_generation.py
@@ -16,7 +16,7 @@ def level_1_challenge(level):
         b = random.choice([*range(-(3*level+3),-(level+1)), *range((level+2), (3*level+4))])
     else:
         a = random.randint((2*level+3), (5*level+5))
-        b = random.choice([*range(-(5*level-3),-(2*level+3)), *range((2*level+4), (5*level-2))])
+        b = random.choice([*range(-(5*level+1),-(2*level)), *range((2*level+1), (5*level+2))])
 
     if b < 0 :
         sign = '-'
@@ -26,12 +26,12 @@ def level_1_challenge(level):
         a += 1
     if sign == '-' and abs(b) > a :
         a, b = b, a
-
+    print(a,b)
     if sign == '-' :
         a = int(a * 1.3)
     x = abs(a+b)
     answer = "{} {} {} = ?".format(abs(a),sign,abs(b))
     return answer, x,
 
-print(level_1_challenge(1))
+print(level_1_challenge(25))
 
