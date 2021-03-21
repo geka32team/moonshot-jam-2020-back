2b83fc2e396be7c3fb04404893a77772d955efc4
Smirnov Sergiy <snoff87@gmail.com>
2021-03-21 12:57:27 +0200

    level 4 ready

--- tests/Level_4_challenge_generation.py
+++ tests/Level_4_challenge_generation.py
@@ -1,101 +1,72 @@
 """Challenge level #4.
-   Returns a task in form {"task": "a +/- x +/- c = d , a +/- b +/- x = d", "result": "x"}
+   Returns a task in form {"task": "(a +/- x) +/- c = d , a +/- (b +/- x) = d", "result": "x"}
 """
 import random
+from Level_1_challenge_generation import get_limits as get_lim
 
-
-def level_4_challenge():
+def level_4_challenge(level):
+    randomizer = random.randint(0, 3)
+    brackets_randomizer = random.randint(0, 1)
     x_place_randomizer = random.randint(0, 2)
-    a = random.randint(1, 5)
-    b = random.randint(-5, 5)
-    c = random.randint(-5, 9)
+    limits = get_lim(level)
+    min = limits[0]
+    max = limits[1]
 
-    if sum((a, b, c)) < 0:
-        if b < 0:
-            a, b = b, a
-        elif c < 0:
-            a, c = c, a
-        a, b, c = -a, -b, -c
+    a = random.randint(min, max)
+    b = random.randint(min, max)
+    c = random.randint(min, max)
 
-    if b < 0:
-        if c < 0:
-            sign1 = '-'
-            sign2 = '-'
-            if x_place_randomizer == 0:
-                x = a
-                a_mod = 'x'
-                b_mod = -b
-                c_mod = -c
-            if x_place_randomizer == 1:
-                x = -b
-                a_mod = a
-                b_mod = 'x'
-                c_mod = -c
-            else:
-                x = -c
-                a_mod = a
-                b_mod = -b
-                c_mod = 'x'
-        else:
-            sign1 = '-'
+    if randomizer == 0:
+        sign1 = '+'
+        sign2 = '+'
+        d = a + b + c
+
+    elif randomizer == 1:
+        sign1 = '+'
+        sign2 = '-'
+        d = a + b - c
+        if d < 0:
+            d, c = c, -d
             sign2 = '+'
-            if x_place_randomizer == 0:
-                x = a
-                a_mod = 'x'
-                b_mod = -b
-                c_mod = c
-            if x_place_randomizer == 1:
-                x = -b
-                a_mod = a
-                b_mod = 'x'
-                c_mod = c
-            else:
-                x = c
-                a_mod = a
-                b_mod = -b
-                c_mod = 'x'
+
+    elif randomizer == 2:
+        sign1 = '-'
+        sign2 = '+'
+        d = a - b + c
+        if d < 0:
+            d, b = b, -d
+            sign1 = '+'
+
+    elif randomizer == 3:
+        sign1 = '-'
+        sign2 = '-'
+        d = a - b - c
+        if d < 0:
+            d, b = b, -d
+            sign1 = '+'
+
+    if x_place_randomizer == 0:
+        x = a
+        a_mod = 'x'
+        b_mod = b
+        c_mod = c
+    elif x_place_randomizer == 1:
+        x = b
+        a_mod = a
+        b_mod = 'x'
+        c_mod = c
     else:
-        if c < 0:
-            sign1 = '+'
-            sign2 = '-'
-            if x_place_randomizer == 0:
-                x = a
-                a_mod = 'x'
-                b_mod = b
-                c_mod = -c
-            if x_place_randomizer == 1:
-                x = b
-                a_mod = a
-                b_mod = 'x'
-                c_mod = -c
-            else:
-                x = -c
-                a_mod = a
-                b_mod = b
-                c_mod = 'x'
-        else:
-            sign1 = '+'
-            sign2 = '+'
-            if x_place_randomizer == 0:
-                x = a
-                a_mod = 'x'
-                b_mod = b
-                c_mod = c
-            if x_place_randomizer == 1:
-                x = b
-                a_mod = a
-                b_mod = 'x'
-                c_mod = c
-            else:
-                x = c
-                a_mod = a
-                b_mod = b
-                c_mod = 'x'
+        x = c
+        a_mod = a
+        b_mod = b
+        c_mod = 'x'
 
-    d = a + b + c
+    if brackets_randomizer == 0:
+        answer = " ({} {} {}) {} {} = {}".format(a_mod, sign1, b_mod, sign2, c_mod, d)
+    else:
+        answer = " {} {} ({} {} {}) = {}".format(a_mod, sign1, b_mod, sign2, c_mod, d)
 
-    answer = " {} {} {} {} {} = {}".format(a_mod, sign1, b_mod, sign2, c_mod, d)
     return answer, x
 
 
-print(level_4_challenge())
+
